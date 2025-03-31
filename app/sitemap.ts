2bd5d796df68506628e5da48e682d5c98a586b99
Priyanshu Chouhan <priynshuchouhn@gmail.com>
2025-03-31 14:24:39 +0530

    URL issue Fixed

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
-    { url: "https://campus-rank.prinshuchouhn.engineer/", lastModified: new Date() },
-    { url: "https://campus-rank.prinshuchouhn.engineer/profile", lastModified: new Date() },
+    { url: "https://campus-rank.priynshuchouhn.engineer/", lastModified: new Date() },
+    { url: "https://campus-rank.priynshuchouhn.engineer/profile", lastModified: new Date() },
   ];
 }
