be0cd06a53faa05c6c8a4ce0fe6f866790ec55d0
Priyanshu Chouhan <priynshuchouhn@gmail.com>
2025-04-03 21:15:09 +0530

    Update robots and sitemap configurations: corrected sitemap URL in robots file and added new URLs for about us, code of conduct, terms of service, and privacy policy in sitemap.

--- app/robots.ts
+++ app/robots.ts
@@ -2,7 +2,8 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: "*", allow: "/" },
-    sitemap: "https://campus-rank.prinshuchouhn.engineer/sitemap.xml",
+    rules: { userAgent: "*", allow: "/" , },
+    sitemap: "https://campus-rank.priynshuchouhn.engineer/sitemap.xml",
+    
   };
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,5 +4,9 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     { url: "https://campus-rank.priynshuchouhn.engineer/", lastModified: new Date() },
     { url: "https://campus-rank.priynshuchouhn.engineer/profile", lastModified: new Date() },
+    { url: "https://campus-rank.priynshuchouhn.engineer/about-us", lastModified: new Date() },
+    { url: "https://campus-rank.priynshuchouhn.engineer/code-of-conduct", lastModified: new Date() },
+    { url: "https://campus-rank.priynshuchouhn.engineer/terms-of-service", lastModified: new Date() },
+    { url: "https://campus-rank.priynshuchouhn.engineer/privacy-policy", lastModified: new Date() },
   ];
 }
