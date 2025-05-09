1d31c6889a0783f6d74db2018bb717c60896509b
Priyanshu Chouhan <priynshuchouhn@gmail.com>
2025-05-09 21:07:14 +0530

    Sitemaps Updated

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -34,12 +34,10 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     { url: "https://campusrank.org/code-of-conduct", lastModified: new Date() },
     { url: "https://campusrank.org/terms-of-service", lastModified: new Date() },
     { url: "https://campusrank.org/privacy-policy", lastModified: new Date() },
-    { url: "https://campusrank.org/dashboard", lastModified: new Date() },
-    { url: "https://campusrank.org/profile", lastModified: new Date() },
-    { url: "https://campusrank.org/goals", lastModified: new Date() },
-    { url: "https://campusrank.org/roadmap", lastModified: new Date() },
-    { url: "https://campusrank.org/roadmap/topics", lastModified: new Date() },
     { url: "https://campusrank.org/blogs", lastModified: new Date() },
+    { url: "https://campusrank.org/get-started", lastModified: new Date() },
+    { url: "https://campusrank.org/contact-us", lastModified: new Date() },
+    { url: "https://campusrank.org/frequently-asked-questions", lastModified: new Date() },
   ];
 
   // Add blog post URLs
